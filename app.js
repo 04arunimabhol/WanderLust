11350cd6fb33afd2d79fbb0cdd0a30d671d666d8
04arunimabhol <bholsoma1@gmail.com>
2026-02-09 01:56:22 +0530

    Initial backend setup

---
+++ app.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+    .then(() => {
+        console.log("Connected to DB");
+    })
+    .catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+
+app.get('/', (req, res) => {
+    res.send("hi, I'm root");
+});
+
+//index route
+app.get('/listings', async (req, res) => {
+    const allListing = await Listing.find({});
+    res.render("listings/index.ejs", {allListing});
+});
+
+//new route
+app.get('/listings/new', async (req, res) => {
+    res.render("listings/new.ejs");
+});
+
+//show route
+app.get('/listings/:id', async (req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", {listing});
+});
+
+//post route
+app.post("/listings", async(req,res) =>{
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+
+//edit route
+app.get('/listings/:id/edit', async (req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", {listing});
+});
+
+//update route
+app.put('/listings/:id', async (req, res) => {
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);
+});
+
+//delete route
+app.delete('/listings/:id', async (req, res) => {
+    let {id} = req.params;
+    const deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+});
+
+app.listen(8080, () => {
+    console.log("Server is running on port 8080");
+});
+
